da1df45543fa45628b81acae4efc1e97855fd1dd
杜晓闪 <xiaoshandu@creditease.cn>
2019-01-07 11:35:31 +0800

    init

---
+++ src/main/java/com/example/emulate/EmulateApplication.java
@@ -0,0 +1,14 @@
+package com.example.emulate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmulateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmulateApplication.class, args);
+    }
+
+}
+
